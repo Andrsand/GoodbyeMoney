2333ed8dad780a236353e70f1ea7efb5b406ce0d
andrsand <angorko@list.ru>
2023-08-27 22:55:20 +0300

    Adding an icon to the category creation form

--- screens/Categories.tsx
+++ screens/Categories.tsx
@@ -1,18 +1,27 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import {
+    Button,
+    KeyboardAvoidingView,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    View,
+   
+} from 'react-native';
 import Entypo from '@expo/vector-icons/Entypo';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 
 import { ListItem } from '../components/ListItem';
-import {
-    TextInput,
-    KeyboardAvoidingView
-} from 'react-native';
 import { theme } from "../theme";
 
 export const Categories = () => {
     return (
-        <KeyboardAvoidingView  behavior='height' keyboardVerticalOffset={112} style={{ margin: 16, flex: 1}}>
+        <KeyboardAvoidingView
+            behavior='padding'
+            keyboardVerticalOffset={112}
+            style={{ margin: 16, flex: 1 }}
+        >
             <View
                 style={{
                     borderRadius: 11,
@@ -35,12 +44,12 @@ export const Categories = () => {
         />
 
             <ListItem label='Erase all data' isDestructive onClick={() => { }} />
-           
             </View>
             <View style={{ flex: 1 }} />
             <View
                 style={{
                     display: 'flex',
+                    alignItems: 'center',
                     flexDirection: 'row',
                     paddingVertical: 8,
                 }}
@@ -50,6 +59,7 @@ export const Categories = () => {
                     placeholder='Category name'
                     style={{
                         color: 'white',
+                        height: 40,
                         borderColor: theme.colors.border,
                         borderWidth: 1,
                         flex: 1,
@@ -57,6 +67,13 @@ export const Categories = () => {
                         paddingLeft: 8,
                     }}
                 />
+                <TouchableOpacity
+                    style={{
+                        padding: 12,
+                    }}
+                >
+                    <FontAwesome name='send' size={24} color={theme.colors.primary}/>
+                </TouchableOpacity>
                 
             </View>
             </KeyboardAvoidingView>
