6dd42fab50dd349dcffbfa3f1fef0fb9c257cb34
andrsand <angorko@list.ru>
2023-09-16 01:21:47 +0300

    bottomSheet manipulation ))

--- screens/Add.tsx
+++ screens/Add.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useMemo, useRef } from 'react';
+// import { Picker } from '@react-native-picker/picker';
 import {
   KeyboardAvoidingView,
   TextInput,
@@ -7,14 +8,25 @@ import {
   TouchableHighlight,
   InputAccessoryView,
   Keyboard,
-  Platform, } from "react-native";
+    Platform,
+} from "react-native";
+import BottomSheet, { BottomSheetFlatList } from '@gorhom/bottom-sheet';
 import { ListItem } from "../components/ListItem";
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import { theme } from '../theme';
 import { Category } from '../models/category';
+import { Recurrence } from '../types/recurrence';
+
+//import DateTimePicker from '@react-native-community/datetimepicker';
+
 
 export const Add = () => {
+    const sheetRef = useRef<BottomSheet>(null);
+    const snapPoints = useMemo(() => ['25%', '50%', '90%'], []);
+
     const [amount, setAmount] = React.useState('');
+    const [recurrence, setRecurrence] = React.useState(Recurrence.None);
+    const [category, setCategory] = React.useState('');
     return (
         <KeyboardAvoidingView
             behavior='padding'
@@ -31,6 +43,7 @@ export const Add = () => {
                     detail={
                  <TextInput
                         placeholder='Amount'
+                        placeholderTextColor={'white'}
                         onChange={(event) => setAmount(event.nativeEvent.text)}
                         value={amount}
                         textAlign='right'
@@ -44,9 +57,22 @@ export const Add = () => {
                 />
             }
             />
-                    
-           
-        </View>
+
+                <ListItem
+                    label='Recurrence' detail={
+                        <TouchableOpacity onPress={() => { }}>
+                            <Text>{recurrence}</Text>
+                        </TouchableOpacity>
+                        }
+                />
+                <BottomSheet ref={sheetRef} snapPoints={snapPoints}>
+                    <BottomSheetFlatList
+                        data={Object.keys(Recurrence)}
+                        keyExtractor={(i) => i}
+                        renderItem={(item) => <Text>{item.item}</Text>}
+                    /> 
+                </BottomSheet>
+            </View>
 
         </KeyboardAvoidingView>
     );
